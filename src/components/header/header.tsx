581dc851e7f773d5cc1a7e271d35dbfcdd36fa70
Fanelesibonge Malaza <malazafanelesibonge@gmail.com>
2023-05-11 18:45:50 +0200

    add header component

---
+++ src/components/header/header.tsx
@@ -0,0 +1,33 @@
+import * as React from "react";
+import {
+  container,
+  container_wrapper,
+  heading,
+  authorName,
+  skills,
+  moreAboutMe,
+} from "../../styles/components/header.module.scss";
+import { StaticImage } from "gatsby-plugin-image";
+
+const Header = () => (
+  <div className={container}>
+    <div className={container_wrapper}>
+      <div className={heading}>
+        <span>HELLO, WORLD</span>
+      </div>
+      <div className={authorName}>
+        <span>I'm Fanelesibonge Malaza.</span>
+      </div>
+      <div className={skills}>Full Stack Software Developer</div>
+      <div className={moreAboutMe}>
+        MORE ABOUT ME{" "}
+        <StaticImage src="../../assets/arrow_down_3.png" alt="downward arrow" />
+      </div>
+    </div>
+    j
+  </div>
+);
+
+// export const Header = () => <title>Fanelesibonge Malaza | Portfolio</title>
+//
+export default Header;
