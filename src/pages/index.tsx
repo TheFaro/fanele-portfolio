c9eee24c47c5c7b55aa438203e25708001b12922
Fanelesibonge Malaza <malazafanelesibonge@gmail.com>
2023-05-11 20:11:45 +0200

    Add about-me element

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,13 @@
 import * as React from "react";
 import Header from "../components/header/header";
+import AboutMe from "../components/about-me/about-me";
 // import { container } from "../styles/index.module.css";
 
 const IndexPage = () => {
   return (
     <div>
       <Header></Header>
+      <AboutMe></AboutMe>
     </div>
   );
 };
