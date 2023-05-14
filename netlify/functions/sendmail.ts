c99fe15f09eb9ae39a6ddc85d0f2ad0588d444d6
Fanelesibonge Malaza <malazafanelesibonge@gmail.com>
2023-05-14 19:23:25 +0200

    update sendmail body

--- netlify/functions/sendmail.ts
+++ netlify/functions/sendmail.ts
@@ -7,41 +7,41 @@ const handler: Handler = async (
   event: HandlerEvent,
   context: HandlerContext
 ) => {
-  // return {
-  //   statusCode: 200,
-  //   body: JSON.stringify({ message: "Hello World" }),
-  // };
-  const data = JSON.parse(event.body!);
-  const { email, subject } = data;
-
-  // console.log(data);
-
-  const body = Object.keys(data)
-    .map((k) => {
-      return `${k}: ${data[k]}`;
-    })
-    .join("<br><br>");
-
-  const mail_to_send = {
-    to: "malazafanelesibonge@gmail.com",
-    from: email,
-    subject: subject ? subject : "New Entry from Portfolio",
-    html: body,
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ message: "Hello World" }),
   };
-
-  try {
-    await sgMail.send(mail_to_send);
-
-    return {
-      statusCode: 200,
-      body: "Message sent successfully",
-    };
-  } catch (e: any) {
-    return {
-      statusCode: e.code,
-      body: e.message,
-    };
-  }
+  // const data = JSON.parse(event.body!);
+  // const { email, subject } = data;
+  //
+  // // console.log(data);
+  //
+  // const body = Object.keys(data)
+  //   .map((k) => {
+  //     return `${k}: ${data[k]}`;
+  //   })
+  //   .join("<br><br>");
+  //
+  // const mail_to_send = {
+  //   to: "malazafanelesibonge@gmail.com",
+  //   from: email,
+  //   subject: subject ? subject : "New Entry from Portfolio",
+  //   html: body,
+  // };
+  //
+  // try {
+  //   await sgMail.send(mail_to_send);
+  //
+  //   return {
+  //     statusCode: 200,
+  //     body: "Message sent successfully",
+  //   };
+  // } catch (e: any) {
+  //   return {
+  //     statusCode: e.code,
+  //     body: e.message,
+  //   };
+  // }
 };
 
 export { handler };
